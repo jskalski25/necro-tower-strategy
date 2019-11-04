854a4171e8f4d3bf807c4afd62cccc74bcfd984c
SzymonHorzela <szymonhorzela@gmail.com>
2019-11-04 11:43:04 +0100

    Dodanie szkieletu class

---
+++ MapMockup1/GridElementsRepo/GridFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapMockup1.GridElementsRepo
+{
+    public class GridFile
+    {
+        public int xPos;
+        public int yPos;
+
+        public ObservableCollection<GridItem> Content;
+    }
+}
