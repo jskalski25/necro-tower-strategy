5408b80de07e3a89a72adbc9074c921e77beee80
BuildTools <unconfigured@null.spigotmc.org>
2019-11-04 14:30:02 +0100

    init

---
+++ Project1/Game.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+
+namespace Project1
+{
+    class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
+        {
+            Load += InitGL;
+        }
+
+        private void InitGL(object sender, System.EventArgs e)
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game(800, 600, "hello world!"))
+            {
+                game.Run(60.0);
+            }
+        }
+    }
+}
